d784b67ac072954c4de5347ec2b0752afc78ac16
Sebastian Krzyszkowiak <dos@dosowisko.net>
2017-07-23 02:09:11 +0200

    initial monitor commit

---
+++ monitor/src/common.c
@@ -0,0 +1,154 @@
+/*! \file common.c
+ *  \brief Common stuff that can be used by all gamestates.
+ */
+/*
+ * Copyright (c) Sebastian Krzyszkowiak <dos@dosowisko.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include <libsuperderpy.h>
+#include <libwebsockets.h>
+
+static int WebSocketCallback( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len ) {
+
+	struct Game *game = user;
+	ALLEGRO_EVENT ev;
+
+	if (game && !game->data->ws_enabled) {
+		return 1; // disconnect
+	}
+
+	switch(reason) {
+		case LWS_CALLBACK_CLIENT_ESTABLISHED:
+			ev.user.type = VETO_EVENT_CONNECTED;
+			al_emit_user_event(&(game->event_source), &ev, NULL);
+			break;
+
+		case LWS_CALLBACK_CLIENT_RECEIVE:
+			ev.user.type = VETO_EVENT_INCOMING_MESSAGE;
+			ev.user.data1 = (intptr_t) in;
+			ev.user.data2 = len;
+			al_emit_user_event(&(game->event_source), &ev, NULL);
+			break;
+
+		case LWS_CALLBACK_CLOSED:
+		case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+			ev.user.type = VETO_EVENT_DISCONNECTED;
+			al_emit_user_event(&(game->event_source), &ev, NULL);
+			break;
+
+		default:
+			break;
+	}
+
+	return 0;
+}
+
+static struct lws_protocols protocols[] = {
+  {
+		.name = "veto",
+		.callback = WebSocketCallback,
+		.rx_buffer_size = 16
+  },
+  { .callback = NULL } /* terminator */
+};
+
+bool GlobalEventHandler(struct Game *game, ALLEGRO_EVENT *event) {
+	lws_service(game->data->ws_context, 0);
+
+	if (event->type == VETO_EVENT_DISCONNECTED) {
+		if (game->data->ws_connected) {
+			PrintConsole(game, "[ws] Disconnected!");
+		} else {
+			PrintConsole(game, "[ws] Connection failed!");
+		}
+		game->data->ws_connected = false;
+		if (game->data->ws_enabled) {
+			// reconnecting
+			game->data->ws_enabled = false;
+			//WebSocketConnect(game); TODO: schedule reconnection
+		}
+	}
+	if (event->type == VETO_EVENT_CONNECTED) {
+		PrintConsole(game, "[ws] Connected!");
+		game->data->ws_connected = true;
+	}
+	if (event->type == VETO_EVENT_INCOMING_MESSAGE) {
+		PrintConsole(game, "[ws] Incoming message (%d): %s", event->user.data2, event->user.data1);
+	}
+	if (event->type == VETO_EVENT_CONNECTING) {
+		PrintConsole(game, "[ws] Connecting...");
+	}
+
+	return false;
+}
+
+void WebSocketConnect(struct Game *game) {
+	if (game->data->ws_enabled) {
+		return;
+	}
+
+	struct lws_context_creation_info info;
+	memset(&info, 0, sizeof(info));
+
+	info.port = CONTEXT_PORT_NO_LISTEN;
+	info.protocols = protocols;
+	info.gid = -1;
+	info.uid = -1;
+
+	game->data->ws_context = lws_create_context(&info);
+
+	struct lws_client_connect_info ccinfo;
+	memset(&ccinfo, 0, sizeof(ccinfo));
+
+	ccinfo.context = game->data->ws_context;
+	ccinfo.address = "localhost";
+	ccinfo.port = 8888;
+	ccinfo.path = "/";
+	ccinfo.host = lws_canonical_hostname(game->data->ws_context);
+	ccinfo.origin = "veto-monitor";
+	ccinfo.protocol = protocols[0].name;
+	ccinfo.ietf_version_or_minus_one = -1;
+	ccinfo.userdata = game;
+
+	game->data->ws_enabled = true;
+
+	ALLEGRO_EVENT ev;
+	ev.user.type = VETO_EVENT_CONNECTING;
+	al_emit_user_event(&(game->event_source), &ev, NULL);
+
+	lws_client_connect_via_info(&ccinfo);
+}
+
+void WebSocketDisconnect(struct Game *game) {
+	if (!game->data->ws_enabled) {
+		return;
+	}
+
+	lws_context_destroy(game->data->ws_context);
+	game->data->ws_enabled = false;
+}
+
+struct CommonResources* CreateGameData(struct Game *game) {
+	struct CommonResources *data = calloc(1, sizeof(struct CommonResources));
+
+	return data;
+}
+
+void DestroyGameData(struct Game *game, struct CommonResources *data) {
+	free(data);
+}
+
---
+++ monitor/src/common.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Sebastian Krzyszkowiak <dos@dosowisko.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define LIBSUPERDERPY_DATA_TYPE struct CommonResources
+#include <libsuperderpy.h>
+#include <libwebsockets.h>
+
+struct CommonResources {
+		// Fill in with common data accessible from all gamestates.
+		struct lws_context *ws_context;
+		bool ws_connected;
+		bool ws_enabled;
+};
+
+typedef enum {
+	VETO_EVENT_INCOMING_MESSAGE = 512,
+	VETO_EVENT_CONNECTING,
+	VETO_EVENT_CONNECTED,
+	VETO_EVENT_DISCONNECTED
+} VETO_EVENT_TYPE;
+
+struct CommonResources* CreateGameData(struct Game *game);
+void DestroyGameData(struct Game *game, struct CommonResources *data);
+void WebSocketConnect(struct Game *game);
+void WebSocketDisconnect(struct Game *game);
+bool GlobalEventHandler(struct Game *game, ALLEGRO_EVENT *event);
